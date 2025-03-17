71019abb46e6f2d02d2bfb8dbc9dc25a61567b03
AdithyanSajan <adithyanarayanansajan@gmail.com>
2025-03-17 14:34:41 +0530

    complete 06,07,08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,42 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+  let lengthOfArray = arr.length;
+  let sumres = 0;
+  for (let i = 0; i < lengthOfArray; i++) {
+    sumres += arr[i];
+  }
+  return sumres
 };
 
-const multiply = function() {
-
+const multiply = function (a) {
+  res = 1;
+  for (let i = 0; i < a.length; i++) {
+    res = res * a[i];
+  }
+  return res;
 };
 
-const power = function() {
-	
+const power = function (base, exponent) {
+  let result = 1;
+  for (let i = 0; i < exponent; i++) {
+    result = result * base;
+  }
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+  let res = 1;
+  for (let i = 1; i <= num; i++) {
+    res = res * i;
+  }
+  return res;
 };
 
 // Do not edit below this line
