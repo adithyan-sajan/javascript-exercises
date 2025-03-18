52011b71d5a9c1112dc6970d9d2457064ad71ed3
Adithyan-Sajan <adithyanarayanansajan@gmail.com>
2025-03-18 10:40:52 +0530

    complete 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
-
+const fibonacci = function (input) {
+    let n1 = 0;
+    let n2 = 1;
+    input = Number(input)
+    if (input === 1)
+        return 1;
+    if (input === 0)
+        return 0;
+    if (input < 0)
+        return "OOPS"
+    for (let i = 1; i < input; i++) {
+        n3 = n1 + n2;
+        n1 = n2;
+        n2 = n3;
+    }
+    return n3;
 };
 
 // Do not edit below this line
