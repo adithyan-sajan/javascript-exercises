9e4c7a5175c48bfbac3863fbc48ae42f7d13b489
Adithyan-Sajan <adithyanarayanansajan@gmail.com>
2025-03-18 10:31:05 +0530

    complete 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (input) {
+    allLowerInput = input.toLowerCase();
+    allLowerInputNoPunctuation = allLowerInput.replace(/[.,'\/#!$%\^&\*;:{}=\-_`~()]/g, "");
+    allLowerInputNoSpace = allLowerInputNoPunctuation.replace(/\s/g, "");;
+    reverse = allLowerInputNoSpace.split('').reverse().join('');
+    // return reverse;
+    if (allLowerInputNoSpace == reverse)
+        return true;
+    else
+        return false;
 };
 
 // Do not edit below this line
